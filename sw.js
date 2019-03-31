1f51995ca43845a6ee8ccaa9ceb917da57c309e1
Renzo Chávez <rlchavezc@outlook.com>
2019-03-30 21:49:08 -0500

    modificaciones de iphone x+

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 //imports
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE    = "static-v3";
-const DYNAMIC_CACHE   = "dynamic-v1";
+const STATIC_CACHE    = "static-v4";
+const DYNAMIC_CACHE   = "dynamic-v2";
 const INMUTABLE_CACHE = "inmutable-v1";
 
 const APP_SHELL = [
@@ -37,6 +37,9 @@ self.addEventListener('activate', e => {
         keys.forEach( key => {
             // static-v4
             if (  key !== STATIC_CACHE && key.includes('static') ) {
+                return caches.delete(key);
+            }
+            if (  key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
                 return caches.delete(key);
             }
         });
