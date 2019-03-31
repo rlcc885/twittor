e906bbf83bd43a6d57e51a7f08ff2fca67b7d0d5
Renzo Chávez <rlchavezc@outlook.com>
2019-03-30 19:44:20 -0500

    Modificación de la ruta del sw

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
+var url = window.location.href;
+var swLocation = '/twittor/sw.js';
 if(navigator.serviceWorker){
-    navigator.serviceWorker.register("/sw.js");
+    if(url.includes('localhost') || url.includes('127.0.0.1') )
+        swLocation = '/sw.js';
+    navigator.serviceWorker.register(swLocation);
 }
 
 // Referencias de jQuery
